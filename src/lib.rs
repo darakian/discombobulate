675a4c109118ecd82ab709d5000fab1a5667e394
Jon Moroney <darakian@gmail.com>
2020-04-22 11:21:55 -0700

    fixup! fixup! More stubbing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,13 @@
 use std::path::Path;
 
 #[non_exhaustive]
-pub Struct Metadata{
+pub struct Metadata{
 
 }
 
 pub fn discombobulate<F: AsRef<Path>>(
     file: F,
     chunk_len: u64,
-    transform: fn([u8]) -> (Option([u8]), Option(Metadata))) -> (){
+    transform: fn([u8]) -> (Option<[u8]>, Option<Metadata>)) -> (){
 
 }
