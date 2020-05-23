f22cabf4120f186a42790c6279cf2f637739cfca
Jon Moroney <darakian@gmail.com>
2020-05-22 21:11:45 -0700

    Seems to work. Need more testing and some way to handle len over 1MB or reject

--- src/lib.rs
+++ src/lib.rs
@@ -12,19 +12,22 @@ pub struct Metadata{
 
 pub struct ChunkIter{
     f: File,
+    chunk_len: u64,
     buffer: [u8; CHUNK_SIZE],
 }
 
 impl ChunkIter{
-    pub fn new(f: File) -> Self{
-        ChunkIter{f: f, buffer: [0;CHUNK_SIZE]}
+    pub fn new(f: File, len: u64) -> Self{
+        ChunkIter{f: f, chunk_len: len, buffer: [0;CHUNK_SIZE]}
     }
 }
 
 impl Iterator for ChunkIter{
     type Item = Result<Vec<u8>, io::Error>;
     fn next(&mut self) -> Option<Result<Vec<u8>, io::Error>>{
-        match self.f.read(&mut self.buffer){
+        match self.f.by_ref()
+            .take(self.chunk_len)
+            .read(&mut self.buffer){
             Ok(i) => {
                 if i == 0 {
                     return None
@@ -40,7 +43,7 @@ pub fn discombobulate<F: AsRef<Path>>(file: F, chunk_len: u64) -> Result<Box<dyn
     match File::open(file){
         Err(e) => {return Err(e)}
         Ok(mut f) => {
-            return Ok(Box::new(ChunkIter::new(f)))
+            return Ok(Box::new(ChunkIter::new(f, chunk_len)))
         }
     }
 }
